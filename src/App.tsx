cb1d6557a557bea1ff01291c95551618bbffe189
paulichdom <paulic.domagoj@gmail.com>
2025-12-18 10:06:06 +0100

    feat: refactor App component to use react-konva for draggable shapes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-function App() {
+import { useState } from 'react';
+import { Stage, Layer, Rect, Circle, Text } from 'react-konva';
+
+export const App = () => {
+  const [rectPos, setRectPos] = useState({ x: 20, y: 50 });
+  const [circlePos, setCirclePos] = useState({ x: 200, y: 100 });
+
   return (
-    <div>
-      <h1>Konva react</h1>
-    </div>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text text="Try to drag shapes" />
+        <Rect
+          x={rectPos.x}
+          y={rectPos.y}
+          width={100}
+          height={100}
+          fill="red"
+          shadowBlur={10}
+          draggable
+          onDragEnd={(e) => {
+            setRectPos({
+              x: e.target.x(),
+              y: e.target.y(),
+            });
+          }}
+        />
+        <Circle
+          x={circlePos.x}
+          y={circlePos.y}
+          radius={50}
+          fill="green"
+          draggable
+          onDragEnd={(e) => {
+            setCirclePos({
+              x: e.target.x(),
+              y: e.target.y(),
+            });
+          }}
+        />
+      </Layer>
+    </Stage>
   )
 }
 
-export default App
