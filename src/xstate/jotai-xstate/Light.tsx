0eaead3aaba11c8ba7eae4a9c2120f428272ac0e
paulichdom <paulic.domagoj@gmail.com>
2026-01-06 10:11:33 +0100

    feat: implement async message fetching and loading state

--- src/xstate/jotai-xstate/Light.tsx
+++ src/xstate/jotai-xstate/Light.tsx
@@ -1,7 +1,8 @@
+import { Suspense } from "react";
 import { createMachine } from "xstate"
 import { atomWithMachine } from "jotai-xstate"
 import { createBrowserInspector } from '@statelyai/inspect';
-import { useAtom } from "jotai"
+import { atom, useAtom } from "jotai"
 
 const { inspect } = createBrowserInspector({
   iframe: document.getElementById('inspector-iframe') as HTMLIFrameElement | null
@@ -27,6 +28,27 @@ const lightAtom = atomWithMachine(lightMachine, {
   inspect
 });
 
+const fetchLightMessage = async (light: string) => {
+  await new Promise((r) => setTimeout(r, 1500))
+  return `Hello ${light}`
+}
+
+const asyncMessageAtom = atom(async (get) => {
+  const light = get(lightAtom).value
+  const message = await fetchLightMessage(light as string);
+  return message
+})
+
+const LightMessage = () => {
+  const [message] = useAtom(asyncMessageAtom)
+
+  return (
+    <text x={100} y={20}>
+      {message}
+    </text>
+  )
+}
+
 export const Light = () => {
   const [state, send] = useAtom(lightAtom);
 
@@ -42,9 +64,25 @@ export const Light = () => {
         cx={40}
         cy={40}
       />
-      <text x={100} y={20}>
-        {state.value as string}
-      </text>
+      <Suspense fallback={<Loader />}>
+        <LightMessage />
+      </Suspense>
     </svg>
   )
 }
+
+const Loader = () => (
+  <g>
+    <circle fill="gray" cx={110} cy={15}>
+      <animate
+        attributeName="r"
+        values="2;8;2"
+        dur='800ms'
+        repeatCount="indefinite"
+      />
+    </circle>
+    <text x={130} y={20} textAnchor="start" fill="black">
+      Loading...
+    </text>
+  </g>
+)
